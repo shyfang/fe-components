4504bd47922e920fee101d41160f2728d8cb9fae
shaoyanfang <shaoyanfang@wecash.net>
2023-02-17 18:18:37 +0800

    feat: npm

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { default as Foo } from './Foo';
 export { default as Button } from './Button';
 export { default as ConfigProvider } from './config-provider';
-
-export { default as Modal } from './modal';
 export { default as Empty } from './empty';
+export { default as Foo } from './Foo';
+export { default as Icon } from './icon';
+export { default as Modal } from './modal';
