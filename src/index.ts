532bb7104a076a8932079aa68b3964d398d68f7c
shaoyanfang <shaoyanfang@wecash.net>
2023-03-06 11:48:24 +0800

    feat: src index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
+/** 1.基础组件 */
+/** 5.待开发 */
 export { default as Button } from './Button';
+/** 4.全局配置 */
 export { default as ConfigProvider } from './config-provider';
 export { default as Empty } from './empty';
 export { default as Foo } from './Foo';
+/** 2.布局组件 */
 export { Col, GridContainer, Row } from './grid';
-export type { ColProps, RowProps } from './grid';
 export { default as Icon } from './icon';
-export type { IconProps } from './icon';
 export { LocaleContext, LocaleReceiver } from './locale';
-export type { Locale as LocaleProps, LocaleContextProps } from './locale';
+/** 3. 操作反馈 */
 export { default as Modal } from './modal';
