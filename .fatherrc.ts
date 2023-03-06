af16b66f2fe568d399d3ac48f18e71eef668bcd9
shaoyanfang <shaoyanfang@wecash.net>
2023-03-06 14:46:45 +0800

    feat: father build ignore demo

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
   esm: {
     output: 'dist',
+    ignores: ['src/**/demo/**'],
   },
   extraBabelPlugins: [
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
